e779972d0177172a84d75337b6e8d14d9c7446a7
osorov97 <osorov.nursultan97@gmail.com>
2022-10-04 15:32:53 +0600

    34

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] nums = {1,3,5,6};
+        int target = 2; //1,4
+        System.out.println(searchInsert(nums,target));
+
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int nachalnaya = 0;
+        int konechnaya = nums.length - 1;
+        while( nachalnaya <= konechnaya ) {
+
+            int seredina = (nachalnaya + konechnaya) / 2;
+            if(nums[seredina] == target) {
+                return seredina;
+            }
+            else if (target > nums[seredina]) {
+                nachalnaya = seredina + 1;
+            }
+            else {
+                konechnaya = seredina - 1;
+            }
+        }
+        return konechnaya + 1;
     }
 }
